545ef0c25c2ea8af43113db1afb30b83d57aa400
杨飞 <ridewind@gmail.com>
2022-05-09 13:22:52 +0800

    minio s3

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 Mount s3 buckets into pods in k8s.
 
 [Here](https://blog.meain.io/2020/mounting-s3-bucket-kube/) is a blog post which explains it in detail.
+
+  enabled: true
+  S3_AK: myminioadmin
+  S3_SK: myminioadmin
+  S3_ENDPOINT: cn-north-1
+  S3_URL: http://192.168.101.213:9000
+  S3_REGION: cn-north-1
+  S3_BUCKET: simplify
--- run.sh
+++ run.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-echo "$AWS_KEY:$AWS_SECRET_KEY" > passwd && chmod 600 passwd
-s3fs "$S3_BUCKET" "$MNT_POINT" -o passwd_file=passwd  && tail -f /dev/null
+echo "$S3_AK:$S3_SK" > .passwd-s3fs && chmod 600 .passwd-s3fs
+s3fs -o passwd_file=.passwd-s3fs -o use_path_request_style -o endpoint="$S3_ENDPOINT" -o url="$S3_URL" -o bucket="$S3_BUCKET" "$MNT_POINT" && tail -f /dev/null
